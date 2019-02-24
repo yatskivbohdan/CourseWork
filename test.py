80d4bc5fd5c6b0f48917aec01b0fad3774deade7
yatskivbohdan <44812624+yatskivbohdan@users.noreply.github.com>
2019-02-24 15:59:58 +0200

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,6 @@ import http.client
 import json
 
 
-#"f197b7b02f4244c4b7080c704dbf4a25": FOOTBALL_DATA_API
 connection = http.client.HTTPConnection('api.football-data.org')
 headers = {'X-Auth-Token': hidden.token, 'X-Response-Control': 'minified'}
 connection.request('GET', '/v2/competitions/PL/scorers', None, headers)
@@ -17,15 +16,3 @@ matches = json.loads(connection.getresponse().read().decode())
 with open("F:/Programming/Labs/CourseWork/results.json", 'w', encoding = 'utf-8') as results:
     for match in matches['matches']:
         json.dump(match, results)
-
-
-
-#print(data.get_available_competitions())
-#data.competition = "premier league"
-#teams_in_premier = data.get_info('teams')
-#print(data.get_info('matches'))
-#from datetime import datetime
-#date_today = datetime.today()
-#matches = data.get_info('matches', dateFrom='2018-08-01', dateTo=date_today)
-#for i in matches["filters"]:
-#    print(i)
