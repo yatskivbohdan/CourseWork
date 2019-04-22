f615725a67cd977c2da517f27f0187b036667e44
yatskivbohdan <44812624+yatskivbohdan@users.noreply.github.com>
2019-04-22 18:06:58 +0300

    Add files via upload

--- ADT/adt.py
+++ ADT/adt.py
@@ -1,1 +1,70 @@
+import json
+
+class MatchList(list):
+    def get_matchday(self, num):
+        matchday = []
+        for el in self:
+            if el[1] == num:
+                matchday.append(el)
+        return MatchList(matchday)
+
+    def get_team_matches(self, team_name):
+        team_matches = []
+        for el in self:
+            if el[2] == team_name or el[3] == team_name:
+                team_matches.append(el)
+        return MatchList(team_matches)
+
+    def get_home_matches(self, team_name):
+        home_matches = []
+        for el in self.get_team_matches(team_name):
+            if el[2] == team_name:
+                home_matches.append(el)
+        return MatchList(home_matches)
+
+    def get_away_matches(self, team_name):
+        away_matches = []
+        for el in self.get_team_matches(team_name):
+            if el[3] == team_name:
+                away_matches.append(el)
+        return MatchList(away_matches)
+
+    def get_stats(self, team_name):
+        wins = 0
+        loses = 0
+        draws = 0
+        for el in self.get_home_matches(team_name):
+            if el[0] == "FINISHED":
+                if el[4]['homeTeam'] > el[4]['awayTeam']:
+                    wins += 1
+                elif el[4]['homeTeam'] == el[4]['awayTeam']:
+                    draws += 1
+                else:
+                    loses += 1
+        for el in self.get_away_matches(team_name):
+            if el[0] == "FINISHED":
+                if el[4]['homeTeam'] > el[4]['awayTeam']:
+                    loses += 1
+                elif el[4]['homeTeam'] == el[4]['awayTeam']:
+                    draws += 1
+                else:
+                    wins += 1
+        return (wins, draws, loses)
+
+    def get_goals(self, team_name):
+        scored = 0
+        missed = 0
+        for el in self.get_home_matches(team_name):
+            if el[0] == "FINISHED":
+                scored += el[4]['homeTeam']
+                missed += el[4]['awayTeam']
+        for el in self.get_away_matches(team_name):
+            if el[0] == "FINISHED":
+                scored += el[4]['awayTeam']
+                missed += el[4]['homeTeam']
+        return(scored, missed)
+
+
+
+
 
---
+++ ADT/adt_test.py
@@ -0,0 +1,33 @@
+import json
+from adt import MatchList
+
+with open("data/PLmatches.json", 'r') as file:
+    matches_js = json.load(file)
+lst = matches_js['matches']
+matches = MatchList()
+for match in lst:
+    matches.append((match['status'], match['matchday'], match['homeTeam']['name'],
+                      match['awayTeam']['name'], match['score']['fullTime']))
+
+# matchday test
+print(matches.get_matchday(35))
+# home matches test
+print(matches.get_home_matches("Liverpool FC"))
+
+# matches between 2 teams test
+liverpool = matches.get_team_matches("Liverpool FC")
+livmc = liverpool.get_team_matches("Manchester City FC")
+print(livmc)
+
+# stats test
+stats_liv = matches.get_stats("Liverpool FC")
+print(stats_liv)
+
+# home stats test
+stats_liv_home = matches.get_home_matches("Liverpool FC").get_stats("Liverpool FC")
+print(stats_liv_home)
+
+# all goals or goals against other team test
+liv_goals = matches.get_goals("Liverpool FC")
+print(liv_goals)
+print(livmc.get_goals("Liverpool FC"))
