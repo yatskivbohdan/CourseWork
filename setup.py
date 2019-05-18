09f7ac27c24b5c806468a29b0d8a4dd60ddac376
yatskivbohdan <44812624+yatskivbohdan@users.noreply.github.com>
2019-05-18 22:59:42 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+setup(
+    name='FootballDataAnalysis',
+    version='1.0.0',
+    author='Bohdan Yatskiv',
+    author_email='yatskiv.b@ucu.edu.ua',
+    url='https://github.com/yatskivbohdan/Football_data_analisys_project',
+    description='this package allows to get detailed team statistics from top 5 football leagues',
+    keywords=['football', 'football statistics'],
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 1 - Alpha",
+        "Environment :: Other Environment",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved ",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Games/Entertainment"
+        ],
+    scripts=['main.py']
+
+
+
+
+)
